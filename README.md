3831e3c3d3ff5b01f83f7d1c8e8933d8f0fc2278
ecabello <erneridesbmx@gmail.com>
2015-06-14 12:39:37 -0400

    Reorganized the code

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # sails-hook-userlogin
+
+Basic User login functionality hook for sails.
+
+It uses passports and a variety of Passport Strategies to allow login using several Social networks
+
+# Passport Strategies
+
+* Local
+* Google
+* Twitter
+* Facebook
+* Github
+
+
+# Routes
+
+The Hook intalls and handle the following routes
+
+* POST /login
+* /google/*
+* /twitter/*
+* /facebook/*
+* /github/*
+* /logout
+
+
+# Configuration
+
+By default the Hook will provide local Strategy login. It would use a user model
+called User and the default login callback will send a json representation of
+the user upon success.
+
+All these can be changed and Social Network login can be enabled by adding a
+userlogin.js file under your sails config directory.
+
+For example to configure login for google and twitter social networks the config
+would look like this.
+
+module.exports.userlogin = {
+  google: {
+    clientID: 'YOUR-GOOGLE-CLIENTID',
+    clientSecret: 'PdgdRaq8VJC6EZjS5-Epf9RF',
+    callbackURL: 'http://localhost:1337/google/return'
+  },
+  twitter: {
+    clientID: 'YOUR-TWITTER-CONSUMER-KEY',
+    clientSecret: 'YOUR-TWITTER-CONSUMER_SECRET',
+    callbackURL: "https//localhost:1337/twitter/return"
+  }
+};
+
+The user model name can be changed by using the 'userModel' setting.
+
+module.exports.userlogin = {
+  userModel: 'Customer'
+};
+
+The Hook will check is the model exists. If it doesnt, the Hook will inject it.
+
+Local login functionality can be suppressed by setting local to false.
+
+module.exports.userlogin = {
+  local: false
+};
--- index.js
+++ index.js
@@ -1,309 +1,143 @@
 var _ = require('lodash'),
-async = require('async'),
-bcrypt = require('bcryptjs'),
-passport = require('passport'),
-LocalStrategy = require('passport-local').Strategy,
-GitHubStrategy = require('passport-github').Strategy,
-FacebookStrategy = require('passport-facebook').Strategy,
-GoogleStrategy = require('passport-google-oauth').OAuth2Strategy,
-TwitterStrategy = require('passport-twitter').Strategy,
-Waterline = require("../sails/node_modules/waterline"),
-_addResViewMethod = require("../sails/lib/hooks/views/res.view.js");
+    authenticate = require('./lib/authenticate'),
+    injectModel = require('./lib/injectmodel'),
+    Waterline = require('sails/node_modules/waterline');
 
 
 module.exports = function userlogin(sails) {
-  sails.log.info('loading userlogin');
+    sails.log.info('loading userlogin' );
 
-  function injectModel(sails, model, cb) {
-    var loadUserModules = require('sails/lib/hooks/orm/load-user-modules')(sails);
-    var normalizeModel = require('sails/lib/hooks/orm/normalize-model')(sails);
-
-    async.auto({
-      load: loadUserModules,
-
-      // normalize model definition
-      normalize: [ 'load', function(cb) {
-        var modelDef = _.defaults({
-          globalId: model.globalId,
-          identity: model.globalId.toLowerCase(),
-          connection: model.connection || sails.config.models.connection
-        }, model);
-        normalizeModel(modelDef, model.globalId.toLowerCase());
-        cb();
-      }],
-
-      instantiateCollections: ['normalize', function(cb) {
-        // reload orm
-        sails.hooks.orm.reload();
-        cb();
-      }]
-    }, cb);
-  }
-
-  return {
-    __configKey__: {
-      local: true,
-      userModel: 'User',
-      loginCallback: function(err, user, network, req, res) {
-        if (err || !user) {
-            sails.log.info(network + ' authentication failed');
-            return res.notFound('No user found');
-        }
-        req.logIn(user, function(err) {
-            if (err)
-              return res.send(err);
-
-            return res.json(user);
-        });
-      }
-    },
-
-    routes: {
-      before: {
-        'POST /login': function(req,res,next) {
-          return sails.hooks.userlogin.localLogin(req,res);
-        },
-
-        '/google/*': function(req,res,next) {
-          return sails.hooks.userlogin.googleLogin(req,res);
-        },
-
-        '/facebook/*': function(req,res,next) {
-          return sails.hooks.userlogin.facebookLogin(req,res);
-        },
-
-        '/twitter/*': function(req,res,next) {
-          return sails.hooks.userlogin.twitterLogin(req,res);
-        },
-
-        '/logout': function(req,res,next) {
-          return sails.hooks.userlogin.logout(req,res);
-        }
-     }
-   },
-
-   configure: function() {
-      sails.log.info('configuring userlogin');
-      sails.config[this.configKey] = sails.config[this.configKey] || {};
-      _.defaults(sails.config[this.configKey], this.__configKey__);
-   },
-
-   initialize: function(cb) {
-     var self = this;
-     sails.on('hook:orm:loaded', function() {
-        sails.log.info('initializing userlogin');
-
-        // Check if the User model already exists
-        if (self.getUserModel())
-          return self.initPassport(cb);
-
-        // Inject a user model
-        var userModel = {
-          globalId: sails.config[self.configKey].userModel,
-          attributes: {
-            provider: {
-              type: 'string',
-              required: true
-            },
-            toJSON: function() {
-              var obj = this.toObject();
-              // There is no valid reason to ever return a password
-              if ('password' in obj)
-                delete obj.password;
-              return obj;
-            }
-          },
-          beforeCreate: function(user, cb) {
-            this.hashPassword(user, cb);
-          },
-          hashPassword: function(user, cb) {
-            // If user has password, store hash instead
-            if ('password' in user) {
-              bcrypt.genSalt(10, function(err, salt) {
-                bcrypt.hash(user.password, salt, function(err, hash) {
-                  if (err) {
-                    console.log(err);
-                    cb(err);
-                  }
-                  else{
-                    user.password = hash;
-                    cb(null, user);
-                  }
+    return _.extend({
+        __configKey__: {
+            local: true,
+            userModel: 'User',
+            loginCallback: function (err, user, provider, req, res) {
+                if (err || !user) {
+                    sails.log.info(provider + ' authentication failed');
+                    return res.notFound('No user found');
+                }
+                req.logIn(user, function (err) {
+                    return err ? res.send(err) : res.json(user);
                 });
-              });
             }
-            else
-              cb(null, user);
-          }
-        };
-        sails.log.info('injecting ' + userModel.globalId + ' model');
-        injectModel(sails, userModel, function() {
-            return self.initPassport(cb);
-        });
-     });
-   },
+        },
 
-   getUserModel: function() {
-     var userModel = sails.config[this.configKey].userModel;
-     if (userModel.toLowerCase() in sails.models)
-        return sails.models[userModel.toLowerCase()];
-     return null;
-   },
+        routes: {
+            before: {
+                'POST /login': function (req, res, next) {
+                    return sails.hooks.userlogin.localLogin(req, res);
+                },
 
-    initPassport: function(cb) {
-      var self = this;
-      passport.serializeUser(function (user, done) {
-        done(null, user.id);
-      });
-      passport.deserializeUser(function (id, done) {
-        self.getUserModel().findOne(id, function (err, user) {
-          if (err)
-            return done(null, null);
-          return done(null, user);
-        });
-      });
+                '/google/*': function (req, res, next) {
+                    return sails.hooks.userlogin.googleLogin(req, res);
+                },
 
-      passport.initStrategies=function() {
-        // if local was requested
-        if (sails.config[self.configKey].local) {
-          // Use the LocalStrategy within Passport.
-          // Strategies in passport require a `verify` function, which accept
-          // credentials (in this case, a username and password), and invoke a callback
-          // with a user object.
-          passport.use(new LocalStrategy(
-            function (username, password, done) {
-              // locate user in the db
-              self.getUserModel().findOne({ username: username }, function (err, user) {
-                if (err)
-                  return done(err);
-                // Couldnt locate user
-                if (!user)
-                  return done(null, false, { message: 'Unknown user ' + username });
-                // Hash compare
-                bcrypt.compare(password, user.password, function(err, res) {
-                  if (!res)
-                    return done(null, false, {message: 'Invalid Password'});
-                  // Hash matched
-                  return done(null, user, {message: 'Logged In Successfully'});
+                  '/facebook/*': function (req, res, next) {
+                    return sails.hooks.userlogin.facebookLogin(req, res);
+                },
+
+                '/twitter/*': function (req, res, next) {
+                    return sails.hooks.userlogin.twitterLogin(req, res);
+                },
+
+                '/logout': function (req, res, next) {
+                    return sails.hooks.userlogin.logout(req, res);
+                }
+            }
+        },
+
+        configure: function () {
+            sails.log.info('configuring userlogin');
+            sails.config[this.configKey] = sails.config[this.configKey] || {};
+            _.defaults(sails.config[this.configKey], this.__configKey__);
+        },
+
+        initialize: function (cb) {
+            var self = this;
+            sails.on('hook:orm:loaded', function () {
+                sails.log.info('initializing userlogin');
+
+                // Check if the User model already exists
+                if (self.getUserModel())
+                    return self.initPassport(cb);
+
+                // Inject a user model otherwise
+                var userModel = {
+                    globalId: sails.config[self.configKey].userModel,
+                    attributes: {
+                        provider: {
+                            type: 'string',
+                            required: true
+                        },
+                        toJSON: function () {
+                            var obj = this.toObject();
+                            // There is no valid reason to ever return a password
+                            if ('password' in obj)
+                                delete obj.password;
+                            return obj;
+                        }
+                    },
+                    beforeCreate: function (user, cb) {
+                        this.hashPassword(user, cb);
+                    },
+                    hashPassword: function (user, cb) {
+                        // If user has password, store hash instead
+                        if ('password' in user) {
+                            bcrypt.genSalt(10, function (err, salt) {
+                                bcrypt.hash(user.password, salt, function (err, hash) {
+                                    if (err) {
+                                        console.log(err);
+                                        cb(err);
+                                    } else {
+                                        user.password = hash;
+                                        cb(null, user);
+                                    }
+                                });
+                            });
+                        } else cb(null, user);
+                    }
+                };
+                sails.log.info('injecting ' + userModel.globalId + ' model');
+                injectModel(sails, userModel, function () {
+                    return self.initPassport(cb);
                 });
-              });
-            }
-          ));
-        }
-
-        // Verify function for all social networks
-        var socialVerify = function(token, tokenSecret, profile, done) {
-          self.getUserModel().findOne({uid: profile.id}, function(err, user) {
-            if (err)
-              return done(err);
-
-            // If the user is found, return it.
-            if (user)
-              return done(null, user, {message: 'Logged In Successfully'});
-
-            // Not found, create one
-            var data = {
-              provider: profile.provider,
-              uid: profile.id,
-              name: profile.displayName
-            };
-
-            if (profile.emails && profile.emails[0] && profile.emails[0].value)
-              data.email = profile.emails[0].value;
-
-            if (profile.name && profile.name.givenName)
-              data.firstname = profile.name.givenName;
-            if (profile.name && profile.name.familyName)
-              data.lastname = profile.name.familyName;
-
-            self.getUserModel().create(data, function(err, user) {
-              sails.log.info(user.name + ', a ' + user.provider + ' user was created');
-              return done(err, user);
             });
-          });
-        };
-        // if github was requested
-        if (sails.config[self.configKey].github) {
-          passport.use(new GitHubStrategy({
-            clientID: sails.config[self.configKey].github.clientID,
-            clientSecret: sails.config[self.configKey].github.clientSecret,
-            callbackURL: sails.config[self.configKey].github.callbackURL
-          }, socialVerify));
+        },
+
+        getUserModel: function () {
+            var userModel = sails.config[this.configKey].userModel;
+            if (userModel.toLowerCase() in sails.models)
+                return sails.models[userModel.toLowerCase()];
+            return null;
+        },
+
+        localLogin: function (req, res) {
+            this.authenticate(req, res, 'local', {}, sails.config.userlogin.loginCallback);
+        },
+
+        githubLogin: function (req, res) {
+            this.authenticate(req, res, 'github', {}, sails.config.userlogin.loginCallback);
+        },
+
+        googleLogin: function (req, res) {
+            this.authenticate(req, res, 'google', {
+                scope: ['https://www.googleapis.com/auth/plus.login', 'email']
+            }, sails.config.userlogin.loginCallback);
+        },
+
+        facebookLogin: function (req, res) {
+            this.authenticate(req, res, 'facebook', {
+                scope: ['email']
+            }, sails.config.userlogin.loginCallback);
+        },
+
+        twitterLogin: function (req, res) {
+            this.authenticate(req, res, 'twitter', {}, sails.config.userlogin.loginCallback);
+        },
+
+        logout: function (req, res) {
+            req.logout();
+            res.redirect('/');
         }
-        // if facebook was requested
-        if (sails.config[self.configKey].facebook) {
-          passport.use(new FacebookStrategy({
-            clientID: sails.config[self.configKey].facebook.clientID,
-            clientSecret: sails.config[self.configKey].facebook.clientSecret,
-            callbackURL: sails.config[self.configKey].facebook.callbackURL
-          }, socialVerify));
-        }
-        // if google was requested
-        if (sails.config[self.configKey].google) {
-          passport.use(new GoogleStrategy({
-            clientID: sails.config[self.configKey].google.clientID,
-            clientSecret: sails.config[self.configKey].google.clientSecret,
-            callbackURL: sails.config[self.configKey].google.callbackURL
-          }, socialVerify));
-        }
-        // if twitter was requested
-        if (sails.config[self.configKey].twitter) {
-          passport.use(new TwitterStrategy({
-            consumerKey: sails.config[self.configKey].twitter.consumerKey,
-            consumerSecret: sails.config[self.configKey].twitter.consumerSecret,
-            callbackURL: sails.config[self.configKey].twitter.callbackURL
-          }, socialVerify));
-        }
-      };
-      return cb();
-    },
-
-    middleware: function(req,res, next) {
-      passport.initialize()(req, res, function() {
-        passport.session()(req, res, function() {
-          passport.initStrategies();
-          return next();
-        });
-      });
-    },
-
-    localLogin: function(req, res) {
-      this.authenticate(req, res, 'local', {}, sails.config.userlogin.loginCallback);
-    },
-
-    githubLogin: function(req, res) {
-      this.authenticate(req, res, 'github', {}, sails.config.userlogin.loginCallback);
-    },
-
-    googleLogin: function(req, res) {
-      this.authenticate(req, res, 'google', {
-        scope: ['https://www.googleapis.com/auth/plus.login', 'email']
-      }, sails.config.userlogin.loginCallback);
-    },
-
-    facebookLogin: function(req, res) {
-      this.authenticate(req, res, 'facebook', {scope: ['email']}, sails.config.userlogin.loginCallback);
-    },
-
-    twitterLogin: function(req, res) {
-      this.authenticate(req, res, 'twitter', {}, sails.config.userlogin.loginCallback);
-    },
-
-    authenticate: function(req, res, strategy, options, callback) {
-      if (!sails.config.userlogin[strategy]) {
-        _addResViewMethod(req, res, function() {
-          return res.notFound(strategy + ' login not configured');
-        });
-      }
-      else
-        passport.authenticate(strategy, options, function(err, user) {
-            return callback(err, user, strategy, req, res);
-        })(req, res);
-    },
-
-    logout: function(req, res) {
-        req.logout();
-        res.redirect('/');
-    },
-  };
+    }, authenticate);
 };
